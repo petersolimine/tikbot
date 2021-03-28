37fecb2a48fafb0f3ce96808582a7a7d6d0cf754
petersolimine <psolimine@tulane.edu>
2021-03-28 19:07:06 -0400

    mobile view

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ fs = require('fs');
 const cookieLogin = async () => {
     try{
         let browser = await puppeteer.launch({
-            args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'],
+            args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process', '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'],
             executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', //local chrome on macOS
             //defaultViewport: defaultViewport,
             headless: false, //set headless to false to view while running
@@ -18,23 +18,41 @@ const cookieLogin = async () => {
         });
         const page = await browser.newPage();
         page.setDefaultNavigationTimeout(30000); //30 seconds
+
+        //jsx-966597281 close
         
+        fs.readFile('./private/myCookies.json', 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            // parse JSON object
+            //console.log(data.cookies);
+            const cookies = JSON.parse(data.toString());
+        
+            listOfCookies = cookies['cookies'];
+            //console.log(listOfCookies);
+            // print JSON object
+            //console.log(cookies);
+        });
+        await page.waitFor(1000);
+        await page.setViewport({ width: 350, height: 700, isMobile: true});
 
-        if(cookieList){
-            await fs.readFile('private/myCookies.json', function (err) {
-                if (err) return console.log(err);
-                console.log('cookieLogin worked');
-              });
+
+
+        try{
+            await page.setCookie(...listOfCookies);
+            await page.waitFor(5000);
+        }catch(err){
+            console.log(err);
         }
+        console.log('done');
 
-
-
-        await page.setCookie(...cookies);
-        await page.waitFor(5000);
-
-        await page.goto("https://www.tiktok.com/login/");
-        console.og('done');
+        await page.goto("https://www.tiktok.com/inbox?lang=en");
+        console.log('logged in');
+        
     }catch(err){
         console.log("failed with below error. Try again... \n",err.message);
     }
 }
+
+cookieLogin();
